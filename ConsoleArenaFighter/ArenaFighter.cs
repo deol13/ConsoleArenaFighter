151a09bf14ad36d14db40654a5d6ee47eba843dc
Shikyo <dennan_lol@hotmail.com>
2021-07-01 14:40:01 +0200

    Add project files.

---
+++ ConsoleArenaFighter/ArenaFighter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleArenaFighter
+{
+    public class ArenaFighter
+    {
+        static void Main(string[] args)
+        {
+            bool continueGame = true;
+
+            //Game Loop
+            while (continueGame)
+            {
+
+            }
+
+            //EndGame(); //Print
+        }
+
+        public void printA()
+        {
+
+        }
+
+        public void GenerateOpponentCharacterName()
+        {
+            
+        }
+    }
+}
